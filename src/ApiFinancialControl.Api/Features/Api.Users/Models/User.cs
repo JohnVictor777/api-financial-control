983d8702243bef41bc257e038f7a98d39d41036f
johnvictor777 <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>
2026-06-08 00:18:03 -0300

    feat: create users feature module and shared models structure

---
+++ src/ApiFinancialControl.Api/Features/Api.Users/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiFinancialControl.Api.Shared.Models;
+
+namespace ApiFinancialControl.Api.Features.Api.Users.Models
+{
+    public class User : BaseEntity
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
