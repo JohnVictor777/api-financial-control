2d8cdaa9d844a26417ad51f152890e006251620c
johnvictor777 <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>
2026-06-09 23:16:10 -0300

    feat(transaction): create transaction module structure

---
+++ src/ApiFinancialControl.Api/Features/API.Transaction/Models/Transaction.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiFinancialControl.Api.Shared.Enums;
+using ApiFinancialControl.Api.Shared.Models;
+
+namespace ApiFinancialControl.Api.Features.API.Transaction.Models
+{
+    public class Transaction : BaseEntity
+    {
+        public Guid AccountId { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
